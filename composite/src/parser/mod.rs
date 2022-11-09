f62c229a34a977879803036f341b7f8c35108f0e
mandrews <09mandrews@gmail.com>
2022-11-09 14:30:12 -0800

    Start implementing the parser

--- composite/src/parser/mod.rs
+++ composite/src/parser/mod.rs
@@ -7,17 +7,62 @@ use sqlparser::{
 };
 
 pub struct Parser {
+    tokens: &[Token],
+    i: usize,
 }
 
 impl Parser {
-    pub fn new() -> Parser {
-        Parser{}
+    pub fn new(tokens: &[Token]) -> Parser {
+        let mut r = Parser{
+            tokens,
+            i: 0,
+        }
+        r.advance(0);
+        return r;
     }
 
-    pub fn parse_schema(&self, tokens: &[Token]) -> Schema {
-        Schema{
-            unparsed: tokens.iter().map(|x| x.clone()).collect(),
-            stmts: Vec::new(),
+    pub fn advance(&mut self, num: usize) {
+        self.i += num;
+        while {
+            match self.current_token() {
+                Token::Whitespace(_) => self.i += 1,
+                _ => break;
+            }
+        }
+    }
+
+    pub fn current_token(&mut self) -> &Token {
+        &self.tokens[self.i]
+    }
+
+    pub fn parse_schema(&mut self) -> Schema {
+    }
+
+    pub fn parse_stmt(&mut self) -> Stmt {
+        let export = as_word(self.current_token()).expect("Statements must begin with words") == "export";
+        if export {
+            self.advance(1);
+        }
+
+        let word = as_word(self.current_token());
+        match w.value.to_uppercase() {
+            "import" => { }
+            "let" => { }
+            _ => { }
+            "extern" => { }
+            "type" => { }
+            "fn" => { }
+        }
+    }
+}
+
+pub fn as_word(token: &Token) -> Option<&str> {
+    match token {
+        Token::Word(w) => {
+            Some(w.value.to_uppercase())
+        }
+        _ => {
+            None
         }
     }
 }
@@ -31,9 +76,9 @@ pub fn tokenize(text: &str) -> Vec<Token> {
 
 pub fn parse(text: &str) -> Schema {
     let tokens = tokenize(text);
-    let parser = Parser::new();
+    let parser = Parser::new(tokens);
 
-    parser.parse_schema(&tokens)
+    parser.parse_schema()
 }
 
 // pub fn parse_sql(text: &str) {
