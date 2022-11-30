7f18dd8ce68db497eef2af26d5b7ec2d19775893
Ankur Goyal <ankrgyl@gmail.com>
2022-11-29 22:44:30 -0800

    Add max buitlin

--- qvm/src/compile/builtin_types.rs
+++ qvm/src/compile/builtin_types.rs
@@ -51,6 +51,7 @@ const BUILTIN_TYPES: &'static [BuiltinType] = &[
 const BUILTIN_FUNCTIONS: &'static str = "
 fn load<R>(file varchar, format varchar) -> [R] = native;
 fn min<R>(value R) -> R = sql;
+fn max<R>(value R) -> R = sql;
 ";
 
 lazy_static! {
