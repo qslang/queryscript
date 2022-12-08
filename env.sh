b76a1a360aeb9ee06dab713f10f14c88a08b31eb
Ankur Goyal <ankrgyl@gmail.com>
2022-12-08 10:36:00 -0800

    Restore env file

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 SRC_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export PATH="${SRC_ROOT}/src/target/debug/:$PATH"
+export PATH="${SRC_ROOT}/target/debug/:$PATH"
 export PS1="(qvm) $PS1"
