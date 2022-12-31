244aeb884acd98298b502c5c6bb721152278c760
Ankur Goyal <ankrgyl@gmail.com>
2022-12-30 23:12:35 -0800

    Add test transformers

--- qvm/src/tests/mod.rs
+++ qvm/src/tests/mod.rs
@@ -1,17 +1,63 @@
 #[cfg(test)]
 mod tests {
     use difference::assert_diff;
+    use std::borrow::Cow;
     use std::collections::BTreeMap;
     use std::ffi::OsStr;
     use std::fmt;
     use std::fs;
     use std::path::{Path, PathBuf};
 
-    use crate::compile;
-    use crate::compile::{Schema, SchemaRef};
+    use crate::ast;
     use crate::parser;
     use crate::runtime;
     use crate::types;
+    use crate::{
+        compile,
+        compile::{Schema, SchemaRef},
+    };
+
+    trait TestTransformer {
+        fn transform_ast<'a>(&self, ast: &'a ast::Schema) -> Cow<'a, ast::Schema>;
+        fn fallback_expr(&self, idx: usize) -> Option<compile::schema::CTypedExpr>;
+    }
+
+    struct IdentityTransformer();
+    impl TestTransformer for IdentityTransformer {
+        fn transform_ast<'a>(&self, ast: &'a ast::Schema) -> Cow<'a, ast::Schema> {
+            Cow::Borrowed(ast)
+        }
+
+        fn fallback_expr(&self, _idx: usize) -> Option<compile::schema::CTypedExpr> {
+            None
+        }
+    }
+
+    struct UnsafeTransformer {
+        pub original: SchemaRef,
+    }
+    impl TestTransformer for UnsafeTransformer {
+        fn transform_ast<'a>(&self, ast: &'a ast::Schema) -> Cow<'a, ast::Schema> {
+            let mut transformed_ast = ast::Schema { stmts: Vec::new() };
+            for stmt in ast.stmts.iter() {
+                transformed_ast.stmts.push(match stmt {
+                    ast::Stmt {
+                        body: ast::StmtBody::Expr(e),
+                        ..
+                    } => ast::Stmt {
+                        body: ast::StmtBody::UnsafeExpr(e.clone()),
+                        ..stmt.clone()
+                    },
+                    o => o.clone(),
+                });
+            }
+            Cow::Owned(transformed_ast)
+        }
+
+        fn fallback_expr(&self, idx: usize) -> Option<compile::schema::CTypedExpr> {
+            Some(self.original.read().unwrap().exprs[idx].get().clone())
+        }
+    }
 
     fn is_schema_file(ext: Option<&str>) -> bool {
         match ext {
@@ -33,29 +79,37 @@ mod tests {
             }
         }
     }
+    #[allow(unused)]
+    #[derive(Debug)]
+    struct TypedValue {
+        pub type_: types::Type,
+        pub value: String,
+    }
+    fn eval_expr(
+        rt: &runtime::Runtime,
+        expr: &compile::schema::CTypedExpr,
+        schema: &SchemaRef,
+    ) -> Result<TypedValue, runtime::RuntimeError> {
+        let expr = expr.to_runtime_type()?;
+
+        let engine_type = runtime::SQLEngineType::DuckDB;
+        let async_ctx = crate::runtime::build_context(&schema, engine_type);
+        let async_expr = expr.clone();
+
+        let value = rt.block_on(async move { runtime::eval(&async_ctx, &async_expr).await })?;
+
+        let type_ = expr.type_.read().unwrap();
+        Ok(TypedValue {
+            type_: type_.clone(),
+            value: format!("{}", value),
+        })
+    }
 
     fn execute_test_schema(
         rt: &runtime::Runtime,
         path: &std::path::Path,
-    ) -> BTreeMap<String, Box<dyn fmt::Debug>> {
-        let mut result = BTreeMap::<String, Box<dyn fmt::Debug>>::new();
-
-        let contents = match fs::read_to_string(&path) {
-            Ok(c) => c,
-            Err(e) => {
-                result.insert("parse_errors".to_string(), Box::new(vec![e]));
-                return result;
-            }
-        };
-
-        let ast = match parser::parse_schema(path.to_str().unwrap(), &contents).as_result() {
-            Ok(ast) => ast,
-            Err(e) => {
-                result.insert("parse_errors".to_string(), Box::new(vec![e]));
-                return result;
-            }
-        };
-
+        transformer: impl TestTransformer,
+    ) -> (SchemaRef, BTreeMap<String, Box<dyn fmt::Debug>>) {
         let parsed_path = Path::new(path);
         let file = parsed_path.to_str().unwrap().to_string();
         let parent_path = parsed_path.parent();
@@ -63,9 +117,29 @@ mod tests {
             Some(p) => p.to_str().map(|f| f.to_string()),
             None => None,
         };
+        let schema = Schema::new(file, folder);
+
+        let mut result = BTreeMap::<String, Box<dyn fmt::Debug>>::new();
+
+        let contents = match fs::read_to_string(&path) {
+            Ok(c) => c,
+            Err(e) => {
+                result.insert("parse_errors".to_string(), Box::new(vec![e]));
+                return (schema, result);
+            }
+        };
+
+        let ast = match parser::parse_schema(path.to_str().unwrap(), &contents).as_result() {
+            Ok(ast) => ast,
+            Err(e) => {
+                result.insert("parse_errors".to_string(), Box::new(vec![e]));
+                return (schema, result);
+            }
+        };
+
+        let ast = transformer.transform_ast(&ast);
 
         let compiler = compile::Compiler::new().expect("Failed to create compiler");
-        let schema = Schema::new(file, folder);
         let mut schema_result = compiler.compile_schema_ast(schema.clone(), &ast);
 
         result.insert(
@@ -86,48 +160,27 @@ mod tests {
             }
         }
 
-        let engine_type = runtime::SQLEngineType::DuckDB;
         let mut exprs = Vec::new();
-        for expr in &schema.read().unwrap().exprs {
-            #[derive(Debug)]
-            #[allow(dead_code)]
-            struct TypedValue {
-                pub type_: types::Type,
-                pub value: String,
+        for (idx, expr) in (&schema.read().unwrap().exprs).iter().enumerate() {
+            match eval_expr(&rt, &expr.get(), &schema) {
+                Ok(e) => exprs.push(Ok(e)),
+                Err(e) => match transformer.fallback_expr(idx) {
+                    Some(expr) => match eval_expr(&rt, &expr, &schema) {
+                        Ok(e) => exprs.push(Ok(e)),
+                        Err(e) => exprs.push(Err(e)),
+                    },
+                    None => exprs.push(Err(e)),
+                },
             }
-
-            let expr = match expr.to_runtime_type() {
-                Ok(e) => e,
-                Err(e) => {
-                    exprs.push(Err(e));
-                    continue;
-                }
-            };
-            let async_ctx = crate::runtime::build_context(&schema, engine_type);
-            let async_expr = expr.clone();
-
-            let value =
-                match rt.block_on(async move { runtime::eval(&async_ctx, &async_expr).await }) {
-                    Ok(e) => e,
-                    Err(e) => {
-                        exprs.push(Err(e));
-                        continue;
-                    }
-                };
-
-            exprs.push(Ok(TypedValue {
-                type_: expr.type_.read().unwrap().clone(),
-                value: format!("{}", value),
-            }));
         }
 
         result.insert("decls".to_string(), Box::new(decls));
         result.insert("queries".to_string(), Box::new(exprs));
-        result
+        (schema, result)
     }
 
     fn test_schema(rt: &runtime::Runtime, path: &std::path::Path) {
-        let result = execute_test_schema(rt, path);
+        let (_schema, result) = execute_test_schema(rt, path, IdentityTransformer());
         let result_str = format!("{:#?}", result);
 
         let mut expected_file = PathBuf::from(path);
@@ -141,6 +194,16 @@ mod tests {
             fs::write(&expected_file, result_str.as_bytes())
                 .expect(format!("Could not write {}", expected_file.display()).as_str());
         }
+
+        // Unfortunately transforming blindly to unsafe mode doesn't quite work, because the types
+        // are not preserved and the aliases can be different. However this is a WIP and still useful
+        // to run manually to see what the differences are.
+        /*
+        let unsafe_transformer = UnsafeTransformer { original: schema };
+        let (_, unsafe_result) = execute_test_schema(rt, path, unsafe_transformer);
+        let unsafe_result_str = format!("{:#?}", unsafe_result);
+        assert_diff!(result_str.as_str(), unsafe_result_str.as_str(), "\n", 0);
+        */
     }
 
     #[test]
