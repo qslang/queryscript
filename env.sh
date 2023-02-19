53f1860955fc51213db79aab1f020a1c20be0208
Ankur Goyal <ankrgyl@gmail.com>
2023-02-18 16:38:55 -0800

    Release workflow (#69)
    Add github workflow for releasing QueryScript (both nightly and with
    tagged releases). The releases show up on both the VSCode marketplace
    and Github's releases. This does not auto-publish to Cargo.

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 SRC_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ps1_old="$PS1"
-source venv/bin/activate
+source $SRC_ROOT/venv/bin/activate
 export PATH="${SRC_ROOT}/target/debug/:$PATH"
 export PS1="(qs) $ps1_old"
